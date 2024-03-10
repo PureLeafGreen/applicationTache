adcb720523ed9cf42f7cf1295d4be3b0d6e8ec0b
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-03-09 21:17:15 -0500

    Ajout d'une gestion d'exception

--- src/main/java/ca/christopher/applicationtache/config/RestExceptionHandler.java
+++ src/main/java/ca/christopher/applicationtache/config/RestExceptionHandler.java
@@ -1,2 +1,16 @@
-package ca.christopher.applicationtache.config;public class RestExceptionHandler {
+package ca.christopher.applicationtache.config;
+
+import ca.christopher.applicationtache.exceptions.AppException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(value = {AppException.class})
+    @ResponseBody
+    public ResponseEntity<Object> handleAppException(AppException e) {
+        return ResponseEntity.status(e.getCode()).body(e.getMessage());
+    }
 }
