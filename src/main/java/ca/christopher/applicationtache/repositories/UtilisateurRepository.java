a416b640b1d8dee25ecb0c8bc0b3dd306c487f7d
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-03-09 21:19:53 -0500

    Ajout d'un nouveau fetch

--- src/main/java/ca/christopher/applicationtache/repositories/UtilisateurRepository.java
+++ src/main/java/ca/christopher/applicationtache/repositories/UtilisateurRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
     @Query("SELECT u FROM Utilisateur u WHERE u.email = ?1")
     Optional<Utilisateur> findByEmail(String email);
+
+    Optional<Utilisateur> findByPhone(String phone);
     @Query("SELECT u FROM Utilisateur u WHERE u.email = ?1 AND u.password = ?2")
     Optional<Utilisateur> findByEmailAndPassword(String email, String password);
 }
