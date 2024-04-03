c176a43dc4fbf352b211c2ac55f0996daeb23602
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-03 19:36:25 -0400

    Ajout du query pour get

--- src/main/java/ca/christopher/applicationtache/repositories/EvenementRepository.java
+++ src/main/java/ca/christopher/applicationtache/repositories/EvenementRepository.java
@@ -2,6 +2,16 @@ package ca.christopher.applicationtache.repositories;
 
 import ca.christopher.applicationtache.modeles.Evenement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.Optional;
 
 public interface EvenementRepository extends JpaRepository<Evenement, Long> {
+    Optional<Evenement> findByDateDebut(String date);
+
+    @Query("SELECT e FROM Evenement e WHERE SUBSTRING(e.dateDebut, 1, 10) = ?1")
+    Collection<Evenement> findAllByDateDebutSameDay(String selectedDate);
+
 }
