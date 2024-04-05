b1f4101ecdc7126f73dbff25d515e99646dfd6eb
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 21:14:26 -0400

    Ajout du DTO Groupe

--- src/main/java/ca/christopher/applicationtache/DTO/GroupeDTO.java
+++ src/main/java/ca/christopher/applicationtache/DTO/GroupeDTO.java
@@ -1,7 +1,34 @@
 package ca.christopher.applicationtache.DTO;
 
-public class GroupeDTO {
+import ca.christopher.applicationtache.modeles.Groupe;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    public void fromDto() {
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GroupeDTO {
+    private long id;
+    private String nom;
+    private String description;
+    private String code;
+    private List<Long> utilisateurs;
+    private Long admin;
+    private List<Long> taches;
+
+    public GroupeDTO(Groupe groupe) {
+        this.id = groupe.getId();
+        this.nom = groupe.getNom();
+        this.description = groupe.getDescription();
+        this.code = groupe.getCode();
+        this.utilisateurs = null;
+        this.admin = null;
+        this.taches = null;
+    }
+
+    public Groupe fromDto() {
+        return new Groupe(nom, description, code);
     }
 }
