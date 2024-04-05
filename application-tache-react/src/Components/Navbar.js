b2eddaa268df24bb405d0764eeb1c8d8333621f4
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-05 18:21:11 -0400

    Modif de la visibiliter des liens

--- application-tache-react/src/Components/Navbar.js
+++ application-tache-react/src/Components/Navbar.js
@@ -54,7 +54,7 @@ function Navbar() {
                                 <a  onClick={function () {navigate("/login")}} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Se connecter</a>
                             </li>
                         }
-                        {user && user.groupe === null && (
+                        {user != null && (
                             <li>
                                 <a onClick={() => navigate("/user/group")} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Groupe</a>
                             </li>
