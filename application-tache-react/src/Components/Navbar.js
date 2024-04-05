b50fe0a4a21476c862fa23086aef1f38c60dfe28
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 21:15:34 -0400

    Ajout d<un lien pour groupe

--- application-tache-react/src/Components/Navbar.js
+++ application-tache-react/src/Components/Navbar.js
@@ -54,6 +54,11 @@ function Navbar() {
                                 <a  onClick={function () {navigate("/login")}} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Se connecter</a>
                             </li>
                         }
+                        { !user ? <></> :
+                            <li>
+                                <a onClick={function () {navigate("/user/group")}} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Groupe</a>
+                            </li>
+                        }
                         {user && (
                             <li className="relative group">
                                 <button onClick={() => setIsUserNavOpen(!isUserNavOpen)} className="flex items-center focus:outline-none">
