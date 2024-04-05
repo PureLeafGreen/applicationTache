3fb4cb905892f492819bc4b423a3093d37d33d44
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 23:41:09 -0400

    Permet de rejoindre un groupe dans la page groupe

--- application-tache-react/src/Components/GroupePage.js
+++ application-tache-react/src/Components/GroupePage.js
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar";
 import React, {useEffect, useState} from "react";
-import {createGroupe} from "../api/GroupeAPI";
+import {createGroupe, joinGroupe} from "../api/GroupeAPI";
 import {toast} from "react-hot-toast";
 import {useUserContext} from "../UserContext";
 
 function GroupePage() {
     const [groupe, setGroupe] = useState({});
-    const [joinGroupe, setJoinGroupe] = useState({});
+    const [toJoinGroupe, setJoinGroupe] = useState({});
     const [create , setCreate] = useState(false);
     const [errors, setError] = useState({});
     const {user, setUser} = useUserContext();
@@ -52,6 +52,28 @@ function GroupePage() {
         });
     }
 
+    function handleJoin() {
+        var errors = {};
+        if (!toJoinGroupe.code) {
+            toast.error("Code du groupe est requis");
+            errors.code = "Code du groupe est requis";
+        }
+        if (Object.keys(errors).length > 0) {
+            setError(errors);
+            return;
+        }
+        joinGroupe(toJoinGroupe.code, user.id)
+        .then(response => {
+            console.log(response.data);
+            toast.success("Groupe rejoint avec succès");
+            setUser({...user, groupe : response.data.id});
+        })
+        .catch(error => {
+            console.log(error.response);
+            toast.error("An error occurred");
+        });
+    }
+
     return (
         <div className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
             <Navbar/>
@@ -75,12 +97,12 @@ function GroupePage() {
                 <div className={"flex flex-col"}>
                     <form className={"flex flex-col bg-white shadow-lg rounded-lg p-4"}>
                         <label htmlFor="code">Code du groupe</label>
-                        <input type="text" id="code" className="mb-2 border-black border-2 rounded" onChange={event => setJoinGroupe({...joinGroupe, code : event.target.value})}/>
-                        <button type={"button"} className="bg-green-500 text-white rounded px-4 py-2 mt-4">Rejoindre</button>
+                        <input type="text" id="code" className="mb-2 border-black border-2 rounded" onChange={event => setJoinGroupe({...toJoinGroupe, code : event.target.value})}/>
+                        <button type={"button"} onClick={function () {handleJoin()}} className="bg-green-500 text-white rounded px-4 py-2 mt-4">Rejoindre</button>
                     </form>
                 </div>
             }
-            <button onClick={function (){handleCreate()}} className="bg-blue-500 text-white rounded px-4 py-2 mt-4">{create ? "Rejoindre" : "Creer"}</button>
+            <button type={"button"} onClick={function (){handleCreate()}} className="bg-blue-500 text-white rounded px-4 py-2 mt-4">{create ? "Rejoindre" : "Creer"}</button>
         </div>
     )
 }
