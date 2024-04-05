a8ae4b5dadf16c5ebb50e80c296a0398e9fab819
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 21:13:32 -0400

    Changement dans l'affichage des evenements

--- application-tache-react/src/Components/EventsPage.js
+++ application-tache-react/src/Components/EventsPage.js
@@ -22,11 +22,11 @@ function EventsPage() {
 
     return (
         <div
-            className={"flex flex-grow flex-col h-screen justify-around items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
+            className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
             <Navbar/>
             <h1 className="text-4xl font-bold mb-4">Tous vos Evements</h1>
             <div className="flex flex-grow flex-col w-1/2 overflow-auto p-4 space-y-4">
-                {evenements && evenements.map((evenement) => (
+                {evenements.length !== 0 ? evenements.map((evenement) => (
                     <div key={evenement.id} className="flex flex-col bg-white shadow-lg rounded-lg p-4">
                         <h2 className="text-2xl font-bold mb-2">{evenement.nom}</h2>
                         <p className="mb-2">{evenement.description}</p>
@@ -34,7 +34,7 @@ function EventsPage() {
                         <p className="mb-2">{evenement.dateFin}</p>
                         <button className="bg-red-500 text-white rounded px-4 py-2 mt-4">Supprimer</button>
                     </div>
-                ))}
+                )) : <p>Aucun événement</p>}
             </div>
         </div>
     );
