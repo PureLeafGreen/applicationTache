6096d61a8d0928047f98cc31feeaf469e87701cb
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-05 14:16:40 -0400

    Ajout d'un throw exception lors de verif token avec le AppException

--- src/main/java/ca/christopher/applicationtache/config/JwtAuthFilter.java
+++ src/main/java/ca/christopher/applicationtache/config/JwtAuthFilter.java
@@ -1,5 +1,7 @@
 package ca.christopher.applicationtache.config;
 
+import ca.christopher.applicationtache.exceptions.AppException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -32,9 +35,18 @@ public class JwtAuthFilter extends OncePerRequestFilter{
                 try {
                     SecurityContextHolder.getContext().setAuthentication(
                             userAuthProvider.valideToken(authElements[1]));
-                } catch (RuntimeException e) {
+                }
+                catch (JWTVerificationException e) {
+                    SecurityContextHolder.clearContext();
+                    throw new AppException("Invalid token", HttpStatusCode.valueOf(401));
+                }
+                catch (AppException e) {
                     SecurityContextHolder.clearContext();
                     throw e;
+                }
+                catch (RuntimeException e) {
+                    SecurityContextHolder.clearContext();
+                    throw new AppException("Internal error", HttpStatusCode.valueOf(500));
                 }
             }
         }
