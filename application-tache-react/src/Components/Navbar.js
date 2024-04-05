0cc216e9bc46807c6a6ced09c37217892fe1aeb3
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-05 14:15:05 -0400

    Modification du nom de variable

--- application-tache-react/src/Components/Navbar.js
+++ application-tache-react/src/Components/Navbar.js
@@ -54,12 +54,12 @@ function Navbar() {
                                 <a  onClick={function () {navigate("/login")}} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Se connecter</a>
                             </li>
                         }
-                        {user && user.group === null && (
+                        {user && user.groupe === null && (
                             <li>
                                 <a onClick={() => navigate("/user/group")} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Groupe</a>
                             </li>
                         )}
-                        {user && user.group !== null && (
+                        {user && user.groupe !== null && (
                             <li>
                                 <a onClick={function () {navigate("/user/mygroup")}} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">My groupe</a>
                             </li>
