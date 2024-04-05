ee2ae878fff66e64f10fc8dc15102b46d75c00cc
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 23:40:19 -0400

    Query pour trouver un groupe par code

--- src/main/java/ca/christopher/applicationtache/repositories/GroupeRepository.java
+++ src/main/java/ca/christopher/applicationtache/repositories/GroupeRepository.java
@@ -2,6 +2,11 @@ package ca.christopher.applicationtache.repositories;
 
 import ca.christopher.applicationtache.modeles.Groupe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface GroupeRepository extends JpaRepository<Groupe, Long> {
+    @Query("SELECT g FROM Groupe g WHERE g.code = ?1")
+    Optional<Groupe> findByCode(String code);
 }
