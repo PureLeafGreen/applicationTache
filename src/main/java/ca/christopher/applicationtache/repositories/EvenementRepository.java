c9701f2ba1565f4eec34f26fb343300b1e67c6a1
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-05 18:17:23 -0400

    Ajout du getAllEventByUser in repo

--- src/main/java/ca/christopher/applicationtache/repositories/EvenementRepository.java
+++ src/main/java/ca/christopher/applicationtache/repositories/EvenementRepository.java
@@ -1,6 +1,7 @@
 package ca.christopher.applicationtache.repositories;
 
 import ca.christopher.applicationtache.modeles.Evenement;
+import ca.christopher.applicationtache.modeles.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +14,6 @@ public interface EvenementRepository extends JpaRepository<Evenement, Long> {
 
     @Query("SELECT e FROM Evenement e WHERE SUBSTRING(e.dateDebut, 1, 10) = ?1")
     Collection<Evenement> findAllByDateDebutSameDay(String selectedDate);
-
+    @Query("SELECT e FROM Evenement e WHERE e.utilisateur = :utilisateur")
+    Collection<Evenement> findAllByUtilisateur(Utilisateur utilisateur);
 }
