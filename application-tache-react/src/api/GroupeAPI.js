56fcc2e9fc9030830861f4d5278092074edd74e4
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 23:40:50 -0400

    Ajout function pour join groupe API frontend

--- application-tache-react/src/api/GroupeAPI.js
+++ application-tache-react/src/api/GroupeAPI.js
@@ -4,3 +4,7 @@ const baseURL = "/groupes";
 export function createGroupe(Groupe) {
     return request('POST', baseURL + "/add", Groupe, true)
 }
+
+export function joinGroupe(code, user) {
+    return request('POST', baseURL + "/join?code="+code+"&userId="+user, null, true)
+}
