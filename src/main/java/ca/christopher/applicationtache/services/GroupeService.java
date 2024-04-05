7b302cfc79ff02b60fbdbf24eb10ca4def107992
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-04 23:08:56 -0400

    Save un groupe et setgroupe pour le user et group

--- src/main/java/ca/christopher/applicationtache/services/GroupeService.java
+++ src/main/java/ca/christopher/applicationtache/services/GroupeService.java
@@ -1,11 +1,15 @@
 package ca.christopher.applicationtache.services;
 
 import ca.christopher.applicationtache.DTO.GroupeDTO;
+import ca.christopher.applicationtache.exceptions.AppException;
 import ca.christopher.applicationtache.modeles.Groupe;
 import ca.christopher.applicationtache.modeles.Utilisateur;
 import ca.christopher.applicationtache.repositories.GroupeRepository;
 import ca.christopher.applicationtache.repositories.UtilisateurRepository;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GroupeService {
@@ -19,8 +23,23 @@ public class GroupeService {
     }
 
     public GroupeDTO saveGroupe(GroupeDTO groupe) {
-        Groupe groupe1 = groupe.fromDto();
-        return new GroupeDTO(groupeRepository.save(groupe1));
+        try {
+            Groupe groupe1 = groupe.fromDto();
+            if (groupe.getUtilisateurs().isEmpty()){
+                throw new AppException("Un groupe doit avoir au moins un utilisateur", HttpStatusCode.valueOf(400));
+            }
+            Utilisateur user = utilisateurRepository.findById(groupe.getUtilisateurs().get(0)).orElseThrow(() -> new AppException("Utilisateur non trouvé" , HttpStatusCode.valueOf(404)));
+            user.setGroupe(groupe1);
+            groupe1.setUtilisateurs(List.of(user));
+            return new GroupeDTO(groupeRepository.save(groupe1));
+        }
+        catch (AppException e) {
+            throw new AppException(e.getMessage(), e.getCode());
+        }
+        catch (IllegalStateException e) {
+            throw new IllegalStateException("Impossible de créer un groupe");
+        }
+
     }
 
     public GroupeDTO getGroupe(Long id) {
