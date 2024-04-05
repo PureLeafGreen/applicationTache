48ec4c18382892095835bb5b648734b76ce88a2a
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-05 18:21:01 -0400

    Affiche tout les groupe et les membres du groupes

--- application-tache-react/src/Components/MyGroupePage.js
+++ application-tache-react/src/Components/MyGroupePage.js
@@ -1,23 +1,24 @@
 import React, {useEffect, useState} from "react";
-import {getGroupe} from "../api/GroupeAPI";
+import {getGroupe, getGroupes} from "../api/GroupeAPI";
 import {useUserContext} from "../UserContext";
 import {toast} from "react-hot-toast";
 import Navbar from "./Navbar";
 
 function MyGroupePage() {
     const {user, setUser} = useUserContext();
-    const [groupe, setGroupe] = useState({});
+    const [groupes, setGroupes] = useState({});
 
     useEffect(() => {
-        getGroupe(user.groupe)
+        console.log(user.groupe);
+        getGroupes(user.groupe)
         .then(response => {
             console.log(response.data);
-            setGroupe(response.data);
+            setGroupes(response.data);
             toast.success("Groupe récupéré avec succès");
         })
         .catch(error => {
             console.log(error.response);
-            toast.error("An error occurred");
+            toast.error(error?.response.data || "An error occurred");
         })
     }, []);
 
@@ -26,18 +27,26 @@ function MyGroupePage() {
         <div className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
             <Navbar/>
             <h1 className={"text-2xl font-bold"}>Page de votre groupe</h1>
-            <div className={"flex flex-col w-1/2 bg-white p-4 rounded-lg shadow-lg"}>
-                <h1 className={"text-2xl font-bold"}>{groupe.nom}</h1>
-                <p className={"text-lg"}>{groupe.description}</p>
-                <p className={"text-lg"}>Code : {groupe.code}</p>
-                <p className={"text-lg"}>Membres : {groupe.utilisateurs ? groupe.utilisateurs.length : 0}</p>
-                {groupe.utilisateurs && groupe.utilisateurs.map((utilisateur, index) => {
-                    return (
-                        <div key={index} className={"flex flex-row"}>
-                            <p className={"text-lg"}>{utilisateur}</p>
-                        </div>
-                    )
-                })}
+            <div className={"flex flex-col w-10/12 items-center"}>
+                {groupes[0] != null ? groupes.map((groupe) => (
+                    <div key={groupe.id} className={"flex flex-col w-1/2 bg-white p-4 rounded-lg shadow-lg mt-4"}>
+                        <h1 className={"text-2xl font-bold"}>{groupe.nom}</h1>
+                        <p className={"text-lg"}>{groupe.description}</p>
+                        <p className={"text-lg"}>Code : {groupe.code}</p>
+                        <p className={"text-lg"}>Membres : {groupe.utilisateurs ? groupe.utilisateurs.length : 0}</p>
+                        {groupe.utilisateurs && groupe.utilisateurs.map((utilisateur, index) => {
+                            return (
+                                <div key={index} className={"flex flex-row"}>
+                                    <p className={"text-lg"}>{utilisateur}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )) :
+                    <div className={"flex flex-col w-1/2 bg-white p-4 rounded-lg shadow-lg"}>
+                        <h1 className={"text-2xl font-bold"}>Aucun groupe</h1>
+                    </div>
+                }
             </div>
         </div>
     )
