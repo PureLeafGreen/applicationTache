d249c3631054550f013e9f1ce396d4f48335e6dd
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-18 18:41:42 -0400

    Ajout de la page pour voir des evenement de groupe

---
+++ application-tache-react/src/Components/GroupeEventsPage.js
@@ -0,0 +1,87 @@
+import Navbar from "./Navbar";
+import React, {useEffect, useState} from "react";
+import Evenement from "../Modeles/Evenement";
+import {useUserContext} from "../UserContext";
+import {deleteEvent, getEventsByGroup, getEventsByUser} from "../api/EventAPI";
+import {toast} from "react-hot-toast";
+import {useLocation} from "react-router-dom";
+
+function GroupeEventsPage() {
+
+    const [evenements, setEvents] = useState([{Evenement}]);
+    const {user} = useUserContext();
+    const location = useLocation();
+    const [selectGroup, setSelectGroup] = useState(null);
+
+    useEffect(() => {
+        if (location.state) {
+            const { groupe } = location.state;
+            setSelectGroup(groupe);
+        }
+    }, [location.state]);
+
+
+    useEffect(() => {
+        console.log(selectGroup);
+        if (selectGroup == null) {
+            return;
+        }
+        getEventsByGroup(selectGroup)
+            .then(response => {
+                if ( typeof response.data === 'string') {
+                    setEvents([]);
+                }
+                else {
+                    setEvents(response.data);
+                    console.log(response.data);
+                }
+            })
+            .catch(error => {
+                console.log(error.response?.data);
+                toast.error(error.response?.data || "An error occurred");
+            });
+    }, [selectGroup]);
+
+    function handleDeleteEvent(eventId)  {
+        deleteEvent(eventId)
+        .then(response => {
+            console.log(response.data);
+            toast.success("Evenement supprimé avec succès");
+            setEvents(evenements.filter(event => event.id !== eventId));
+        })
+        .catch(error => {
+            console.log(error.response.data);
+            toast.error(error.response?.data || "An error occurred");
+        });
+    }
+
+    return (
+        <div
+            className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
+            <Navbar/>
+            <h1 className="text-4xl font-bold mb-4">Evenements de votre groupe</h1>
+            <div className="flex flex-grow flex-col w-1/2 overflow-auto p-4 space-y-4">
+                {evenements[0].id != null ?
+                    evenements.map((evenement) => (
+
+                        <div key={evenement.id} className="flex flex-col bg-white shadow-lg rounded-lg p-4">
+                            <h2 className="text-2xl font-bold mb-2">{evenement.nom}</h2>
+                            <p className="mb-2">{evenement.description}</p>
+                            <p className="mb-2">{evenement.dateDebut}</p>
+                            <p className="mb-2">{evenement.dateFin}</p>
+                            <button type={"button"} onClick={function () {
+                                handleDeleteEvent(evenement.id)
+                            }} className="bg-red-500 text-white rounded px-4 py-2 mt-4">Supprimer
+                            </button>
+                        </div>
+                    )) :
+                    <div className="flex flex-col bg-white shadow-lg rounded-lg p-4">
+                        <p className="text-2xl font-bold mb-2">Aucun événement</p>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+}
+
+export default GroupeEventsPage;
