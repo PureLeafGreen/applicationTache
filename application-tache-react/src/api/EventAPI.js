0abc4c8132d5e502cd0eaf3c8a274d5953623ae8
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-18 18:41:17 -0400

    Permet dajouter et get les evenements par groupe dans lapi FE

--- application-tache-react/src/api/EventAPI.js
+++ application-tache-react/src/api/EventAPI.js
@@ -4,12 +4,20 @@ const baseURL = "/evenement";
 export function createEvent(Event) {
     return request('POST', baseURL + "/add", Event, true)
 }
+
+export function createEventWithGroup(Event, group) {
+    return request('POST', baseURL + `/addWithGroup?groupid=${group}`, Event, true)
+}
 export function getEvents() {
     return request('GET', baseURL + "/getAll", null, true)
 }
 
 export function getEventsByUser(userId) {
     return request('GET', baseURL + `/getAllByUser?userid=${userId}`, null, true)
+}
+
+export function getEventsByGroup(groupId) {
+    return request('GET', baseURL + `/getAllByGroup?groupid=${groupId}`, null, true)
 }
 
 export function getAllEventsByDate(dateStr) {
