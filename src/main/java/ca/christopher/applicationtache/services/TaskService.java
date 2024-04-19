4406c503c39fd5782135e6b81f91385c34ea741b
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-19 13:02:28 -0400

    Service pour tache BE permet dajouter et de get taches avec groupe

--- src/main/java/ca/christopher/applicationtache/services/TaskService.java
+++ src/main/java/ca/christopher/applicationtache/services/TaskService.java
@@ -2,9 +2,11 @@ package ca.christopher.applicationtache.services;
 
 import ca.christopher.applicationtache.DTO.TaskDTO;
 import ca.christopher.applicationtache.exceptions.AppException;
+import ca.christopher.applicationtache.modeles.Groupe;
 import ca.christopher.applicationtache.modeles.Tache;
 import ca.christopher.applicationtache.modeles.TaskStatus;
 import ca.christopher.applicationtache.modeles.Utilisateur;
+import ca.christopher.applicationtache.repositories.GroupeRepository;
 import ca.christopher.applicationtache.repositories.TaskRepository;
 import ca.christopher.applicationtache.repositories.UtilisateurRepository;
 import org.springframework.http.HttpStatusCode;
@@ -20,9 +22,12 @@ public class TaskService {
 
     private final UtilisateurRepository utilisateurRepository;
 
-    public TaskService(TaskRepository taskRepository, UtilisateurRepository utilisateurRepository) {
+    private final GroupeRepository groupeRepository;
+
+    public TaskService(TaskRepository taskRepository, UtilisateurRepository utilisateurRepository, GroupeRepository groupeRepository) {
         this.taskRepository = taskRepository;
         this.utilisateurRepository = utilisateurRepository;
+        this.groupeRepository = groupeRepository;
     }
 
     public TaskDTO saveTask(TaskDTO task) {
@@ -55,6 +60,41 @@ public class TaskService {
         }
         catch (IllegalStateException e) {
             throw new IllegalStateException("Impossible de trouver les tâches de l'utilisateur");
+        }
+    }
+
+    public TaskDTO saveTaskWithGroup(TaskDTO task, Long groupId) {
+        try {
+            Utilisateur utilisateur = utilisateurRepository.findById(task.getUtilisateur()).orElseThrow(() -> new AppException("Utilisateur non trouvé", HttpStatusCode.valueOf(404)));
+            Groupe groupe = groupeRepository.findById(groupId).orElseThrow(() -> new AppException("Groupe non trouvé", HttpStatusCode.valueOf(404)));
+            Tache tache = task.fromDto();
+            tache.setUtilisateur(utilisateur);
+            tache.setType(TaskStatus.TODO);
+            tache.setGroupe(groupe);
+            return new TaskDTO(taskRepository.save(tache));
+        }
+        catch (AppException e) {
+            throw new AppException(e.getMessage(),e.getCode());
+        }
+        catch (IllegalStateException e) {
+            throw new IllegalStateException("Impossible de créer une tâche");
+        }
+    }
+
+    public List<TaskDTO> getAllByGroup(Long groupId) {
+        try {
+            Groupe groupe = groupeRepository.findById(groupId).orElseThrow(() -> new AppException("Groupe non trouvé", HttpStatusCode.valueOf(404)));
+            List<TaskDTO> taches = taskRepository.findByGroupe(groupe).stream().map(TaskDTO::new).toList();
+            if (taches.isEmpty()) {
+                throw new AppException("Aucune tâche trouvée", HttpStatusCode.valueOf(404));
+            }
+            return taches;
+        }
+        catch (AppException e) {
+            throw new AppException(e.getMessage(), e.getCode());
+        }
+        catch (IllegalStateException e) {
+            throw new IllegalStateException("Impossible de trouver les tâches du groupe");
         }
     }
 }
