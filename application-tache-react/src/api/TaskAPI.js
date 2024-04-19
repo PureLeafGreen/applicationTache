dc1ce2febea485b9a106e29c2553dc15aad1e6d9
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-19 13:03:33 -0400

    Ajout de lapi pour creer et voir les tache de groupe FE

--- application-tache-react/src/api/TaskAPI.js
+++ application-tache-react/src/api/TaskAPI.js
@@ -8,3 +8,11 @@ export function createTask(task) {
 export function getTasksByUser(userId) {
     return request('GET', baseURL + `/getAllByUser?userId=${userId}`, null, true)
 }
+
+export function getTasksByGroup(groupId) {
+    return request('GET', baseURL + `/getAllByGroup?groupId=${groupId}`, null, true)
+}
+
+export function createTaskByGroup(task, group) {
+    return request('POST', baseURL + `/addWithGroup?groupId=${group}`, task, true)
+}
