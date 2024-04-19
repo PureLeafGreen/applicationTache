70b8c9f5b7f3e83f75778a607748e421999946f0
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-19 16:29:05 -0400

    Ajout dun bouton back et modif du style pour etre plus UI friendly lors de la creation de tache perso

--- application-tache-react/src/Components/MyTaskForm.js
+++ application-tache-react/src/Components/MyTaskForm.js
@@ -62,21 +62,31 @@ function MyTaskForm() {
             <h1 className={"text-2xl font-bold"}>Ajouter une taches</h1>
             <form className={"flex flex-col w-1/2 bg-white p-4 rounded-lg shadow-lg mt-4"}>
                 <label className={"text-lg"}>Nom</label>
-                <input type={"text"} className={"border-2 p-2 rounded-lg"} value={task.nom} onChange={(e) => setTask({...task, nom: e.target.value})}/>
+                <input type={"text"} className={"border-2 p-2 rounded-lg"} value={task.nom}
+                       onChange={(e) => setTask({...task, nom: e.target.value})}/>
                 {errors.title && <p className={"text-red-500"}>{errors.title}</p>}
                 <label className={"text-lg"}>Description</label>
-                <input type={"text"} className={"border-2 p-2 rounded-lg"} value={task.description} onChange={(e) => setTask({...task, description: e.target.value})}/>
+                <input type={"text"} className={"border-2 p-2 rounded-lg"} value={task.description}
+                       onChange={(e) => setTask({...task, description: e.target.value})}/>
                 {errors.description && <p className={"text-red-500"}>{errors.description}</p>}
                 <label className={"text-lg"}>Date de début</label>
-                <input type={"datetime-local"} className={"border-2 p-2 rounded-lg"} value={task.dateDebut} onChange={(e) => setTask({...task, dateDebut: e.target.value})}/>
+                <input type={"datetime-local"} className={"border-2 p-2 rounded-lg"} value={task.dateDebut}
+                       onChange={(e) => setTask({...task, dateDebut: e.target.value})}/>
                 {errors.start && <p className={"text-red-500"}>{errors.start}</p>}
                 <label className={"text-lg"}>Date de fin</label>
-                <input type={"datetime-local"} className={"border-2 p-2 rounded-lg"} value={task.dateFin} onChange={(e) => setTask({...task, dateFin: e.target.value})}/>
+                <input type={"datetime-local"} className={"border-2 p-2 rounded-lg"} value={task.dateFin}
+                       onChange={(e) => setTask({...task, dateFin: e.target.value})}/>
                 {errors.end && <p className={"text-red-500"}>{errors.end}</p>}
                 <label className={"text-lg"}>Couleur</label>
-                <input type={"color"} className={"border-2 p-2 rounded-lg"} value={task.couleur} onChange={(e) => setTask({...task, couleur: e.target.value})}/>
-                <button type={"button"} className={"bg-blue-500 text-white p-2 rounded-lg"} onClick={saveTask}>Ajouter</button>
+                <input type={"color"} className={"border-2 p-2 rounded-lg"} value={task.couleur}
+                       onChange={(e) => setTask({...task, couleur: e.target.value})}/>
+                <button type={"button"} className={"bg-blue-500 text-white p-2 rounded-lg"} onClick={saveTask}>Ajouter
+                </button>
             </form>
+            <button className={"p-4 m-4 bg-red-500 text-white rounded-lg"} onClick={function () {
+                window.history.back()
+            }}>Retour
+            </button>
         </div>
     )
 }
