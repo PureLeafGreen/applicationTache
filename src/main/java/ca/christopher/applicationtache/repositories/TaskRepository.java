e197bd8acff069acb3c59f61d8d363fe73731371
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-19 16:24:54 -0400

    Modif des query pour avoir les taches des users qui ne sont pas dans un groupe

--- src/main/java/ca/christopher/applicationtache/repositories/TaskRepository.java
+++ src/main/java/ca/christopher/applicationtache/repositories/TaskRepository.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Tache, Long> {
-    @Query("SELECT t FROM Tache t WHERE t.utilisateur = ?1")
+    @Query("SELECT t FROM Tache t WHERE t.utilisateur = ?1 and t.groupe is null")
     List<Tache> findByUtilisateur(Utilisateur utilisateur);
     @Query("SELECT t FROM Tache t WHERE t.groupe = ?1")
     List<Tache> findByGroupe(Groupe group);
