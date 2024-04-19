9b60a7577329ab772d2bc7dc61ce1152fd823322
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-19 16:27:21 -0400

    Modif du style de la visualisation devenements de groupe pour etre plus UI friendly

--- application-tache-react/src/Components/GroupeEventsPage.js
+++ application-tache-react/src/Components/GroupeEventsPage.js
@@ -1,15 +1,14 @@
 import Navbar from "./Navbar";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Evenement from "../Modeles/Evenement";
-import {useUserContext} from "../UserContext";
-import {deleteEvent, getEventsByGroup, getEventsByUser} from "../api/EventAPI";
-import {toast} from "react-hot-toast";
-import {useLocation} from "react-router-dom";
+import { useUserContext } from "../UserContext";
+import { deleteEvent, getEventsByGroup } from "../api/EventAPI";
+import { toast } from "react-hot-toast";
+import { useLocation } from "react-router-dom";
 
 function GroupeEventsPage() {
-
-    const [evenements, setEvents] = useState([{Evenement}]);
-    const {user} = useUserContext();
+    const [evenements, setEvents] = useState([]);
+    const { user } = useUserContext();
     const location = useLocation();
     const [selectGroup, setSelectGroup] = useState(null);
 
@@ -20,20 +19,16 @@ function GroupeEventsPage() {
         }
     }, [location.state]);
 
-
     useEffect(() => {
-        console.log(selectGroup);
         if (selectGroup == null) {
             return;
         }
         getEventsByGroup(selectGroup)
             .then(response => {
-                if ( typeof response.data === 'string') {
+                if (typeof response.data === 'string') {
                     setEvents([]);
-                }
-                else {
+                } else {
                     setEvents(response.data);
-                    console.log(response.data);
                 }
             })
             .catch(error => {
@@ -42,43 +37,40 @@ function GroupeEventsPage() {
             });
     }, [selectGroup]);
 
-    function handleDeleteEvent(eventId)  {
+    function handleDeleteEvent(eventId) {
         deleteEvent(eventId)
-        .then(response => {
-            console.log(response.data);
-            toast.success("Evenement supprimé avec succès");
-            setEvents(evenements.filter(event => event.id !== eventId));
-        })
-        .catch(error => {
-            console.log(error.response.data);
-            toast.error(error.response?.data || "An error occurred");
-        });
+            .then(response => {
+                console.log(response.data);
+                toast.success("Evenement supprimé avec succès");
+                setEvents(evenements.filter(event => event.id !== eventId));
+            })
+            .catch(error => {
+                console.log(error.response.data);
+                toast.error(error.response?.data || "An error occurred");
+            });
     }
 
     return (
-        <div
-            className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
-            <Navbar/>
+        <div className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
+            <Navbar />
             <h1 className="text-4xl font-bold mb-4">Evenements de votre groupe</h1>
             <div className="flex flex-grow flex-col w-1/2 overflow-auto p-4 space-y-4">
-                {evenements[0].id != null ?
+                {evenements.length > 0 ?
                     evenements.map((evenement) => (
-
                         <div key={evenement.id} className="flex flex-col bg-white shadow-lg rounded-lg p-4">
                             <h2 className="text-2xl font-bold mb-2">{evenement.nom}</h2>
                             <p className="mb-2">{evenement.description}</p>
                             <p className="mb-2">{evenement.dateDebut}</p>
                             <p className="mb-2">{evenement.dateFin}</p>
-                            <button type={"button"} onClick={function () {
-                                handleDeleteEvent(evenement.id)
-                            }} className="bg-red-500 text-white rounded px-4 py-2 mt-4">Supprimer
-                            </button>
+                            <p className="mb-2">Créé par : {evenement.utilisateur ? evenement.utilisateur.nom : "Utilisateur inconnu"}</p>
+                            <button type={"button"} onClick={() => handleDeleteEvent(evenement.id)} className="bg-red-500 text-white rounded px-4 py-2 mt-4">Supprimer</button>
                         </div>
                     )) :
                     <div className="flex flex-col bg-white shadow-lg rounded-lg p-4">
                         <p className="text-2xl font-bold mb-2">Aucun événement</p>
                     </div>
                 }
+                <button className={"p-4 m-4 bg-red-500 text-white rounded-lg"} onClick={() => window.history.back()}>Retour</button>
             </div>
         </div>
     );
