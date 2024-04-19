30872a42bc22b5146917728bd9dcd24713b3b424
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-04-19 16:26:53 -0400

    Modif du style de la creation devenements de groupe pour etre plus UI friendly

--- application-tache-react/src/Components/GroupeEventsFormPage.js
+++ application-tache-react/src/Components/GroupeEventsFormPage.js
@@ -70,22 +70,22 @@ function GroupeEventsFormPage() {
             className={"flex flex-grow flex-col h-screen items-center bg-gradient-to-r from-blue-300 to-gray-500"}>
             <Navbar/>
             <h1 className="text-4xl font-bold mb-4">Créer un événement pour le groupe </h1>
-            <div className={"flex flex-grow flex-col justify-start items-center"}>
-                <form className={"flex flex-col justify-around items-center"}>
+            <div className={"flex flex-grow flex-col justify-start items-center w-full"}>
+                <form className={"flex flex-col w-1/2 bg-white p-4 rounded-lg shadow-lg mt-4"}>
                     <label htmlFor="nom">Titre de l'événement</label>
-                    <input type="text" id="nom" name="nom" onChange={(e) => setEvent({...event, nom: e.target.value})}
+                    <input className={"border-2 p-2"} type="text" id="nom" name="nom" onChange={(e) => setEvent({...event, nom: e.target.value})}
                            required/>
                     {errors.title && <span className="text-red-500">{errors.title}</span>}
                     <label htmlFor="description">Description de l'événement</label>
-                    <textarea id="description" name="description"
+                    <textarea className={"border-2 p-2"} id="description" name="description"
                               onChange={(e) => setEvent({...event, description: e.target.value})} required/>
                     {errors.description && <span className="text-red-500">{errors.description}</span>}
                     <label htmlFor="start">Date de début</label>
-                    <input type="datetime-local" id="start" name="start"
+                    <input  className={"border-2 p-2"} type="datetime-local" id="start" name="start"
                            onChange={(e) => setEvent({...event, dateDebut: e.target.value})} required/>
                     {errors.start && <span className="text-red-500">{errors.start}</span>}
                     <label htmlFor="end">Date de fin</label>
-                    <input type="datetime-local" id="end" name="end"
+                    <input className={"border-2 p-2"} type="datetime-local" id="end" name="end"
                            onChange={(e) => setEvent({...event, dateFin: e.target.value})} required/>
                     {errors.end && <span className="text-red-500">{errors.end}</span>}
                     <button type="button" onClick={function () {
