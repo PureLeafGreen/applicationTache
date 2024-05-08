8ade487826fdab5da2fcc5b6022c16c5b3e4301c
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-05-08 18:45:01 -0400

    Modif du ChatController pour gerer les deux endpoint

--- src/main/java/ca/christopher/applicationtache/controller/ChatController.java
+++ src/main/java/ca/christopher/applicationtache/controller/ChatController.java
@@ -2,7 +2,9 @@ package ca.christopher.applicationtache.controller;
 
 import ca.christopher.applicationtache.modeles.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @CrossOrigin(origins = "http://localhost:3000")
 public class ChatController {
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public Message sendMessage(Message message) {
+    private SimpMessagingTemplate simpMessagingTemplate;
+    @MessageMapping("/message")
+    @SendTo("/topic/public")
+    public Message reveivePublicMessage(@Payload Message message) {
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);
         return message;
     }
 }
