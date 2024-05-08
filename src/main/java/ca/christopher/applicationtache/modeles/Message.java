70afdd4f400aacf55af3ddcdfbf22560a3f93552
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2024-05-08 18:45:16 -0400

    modif de la classe message pour plus de precision

--- src/main/java/ca/christopher/applicationtache/modeles/Message.java
+++ src/main/java/ca/christopher/applicationtache/modeles/Message.java
@@ -3,14 +3,19 @@ package ca.christopher.applicationtache.modeles;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Message {
 
-    private String content;
-    private String sender;
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
 
     // getters and setters
 }
